24c59b44d4c2c7d922f2fa57a297b28e138a4608
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2022-07-27 22:05:11 +0200

    Update script.ps1

--- script/script.ps1
+++ script/script.ps1
@@ -12,11 +12,18 @@ if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
 }
 
 # Récupération du nom d'utilisateur et de l'heure limite
+$limit = @(1, 2, 3, 4, 5, 6, 7)
 Write-Output "Renseignez le nom d'utilisateur cible tel qu'il apparait dans le fichier 'C:\Users'."
 $user = Read-Host "Nom d'utilisateur"
 Write-Output "Renseignez l'heure d'arret voulue au format hhmm (Par exempe, 22h30 se note 2230)."
 Write-Output "Pour desactiver l'arret programme, entrez '0'."
-$limit = Read-Host "Heure de l'arret programme"
+$limit[0] = Read-Host "Lundi"
+$limit[1] = Read-Host "Mardi"
+$limit[2] = Read-Host "Mercredi"
+$limit[3] = Read-Host "Jeudi"
+$limit[4] = Read-Host "Vendredi"
+$limit[5] = Read-Host "Samedi"
+$limit[6] = Read-Host "Dimanche"
 Write-Output " "
 Write-Output "Creation / mise a jour du virus ..."
 
@@ -32,13 +39,16 @@ $launcherContent = 'command = "powershell.exe -nologo -command ' + $scriptPath +
 set shell = CreateObject("WScript.Shell")
 shell.Run command,0'
 # Contenu du script
-$scriptContent = 'while (1) {
-    # Heure limite
-    $limit = ' + $limit + '
+$scriptContent = 'while (1) {# Tableau d heure limite
+    $limitArray = @('+ $limit[0] + ',' + $limit[1] + ',' + $limit[2] + ',' + $limit[3] + ',' + $limit[4] + ',' + $limit[5] + ',' + $limit[6] + ')
     # Obtenir l heure actuelle
     $time = Get-Date -Format "HHmm"
     # Convertir en entier
     $time = $time -as [int]
+    # Obtenir jour de la semaine
+    $day = [Int] (Get-Date).DayOfWeek
+    # Heure limite aujourdhui
+    $limit = $limitArray[$day-1]
     # Si l heure actuelle est supérieure à la limite ou inférieure à 6h00 et différente de 0, éteindre l ordi
     if (($time -gt $limit) -or ($time -lt 0600) -and ($time)) {
         Stop-Computer -ComputerName localhost -Force
@@ -81,7 +91,7 @@ else {
 }
 Write-Output "Ok."
 
-if ($restart){
+if ($restart) {
     Write-Output "REDEMARRAGE REQUIS"
 }
 
